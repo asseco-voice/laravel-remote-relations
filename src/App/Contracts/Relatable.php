93ecb7cfd4a9fc5311f36a8584db10d1062ef41f
AkronimBlack <bojan.madzarac@asseco-see.com>
2021-02-04 08:51:52 +0100

    Add Relatable interface

---
+++ src/App/Contracts/Relatable.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Asseco\RemoteRelations\App\Contracts;
+
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Model;
+
+interface Relatable
+{
+    public function remoteRelations(): MorphMany;
+    public function relate(string $service, string $model, $id): Model;
+    public function relateQuietly(string $service, string $model, $id): Model;
+    public function unrelate(string $service, string $model, $id, bool $force = false): void;
+    public function unrelateQuietly(string $service, string $model, $id, bool $force = false): void;
+}
