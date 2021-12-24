b135a6bf935b885557d372b6d3891e497103fcfd
Josip Milotić <42002911+josip-milotic@users.noreply.github.com>
2021-12-24 13:16:22 +0100

    Revert relatable interface #patch (#27)
    * Revert interface delete
    
    * Apply fixes from StyleCI
    
    * Minor
    
    Co-authored-by: jmilotic <josip.milotic@asseco-see.hr>
    Co-authored-by: Marko Dupor <Norgul@users.noreply.github.com>

---
+++ src/App/Contracts/Relatable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Asseco\RemoteRelations\App\Contracts;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+
+interface Relatable
+{
+    public function remoteRelations(): MorphMany;
+
+    public function relate(string $service, string $model, $id, bool $acknowledged = false): Model;
+
+    public function relateQuietly(string $service, string $model, $id, bool $acknowledged = false): Model;
+
+    public function unrelate(string $service, string $model, $id, bool $force = false): void;
+
+    public function unrelateQuietly(string $service, string $model, $id, bool $force = false): void;
+}
