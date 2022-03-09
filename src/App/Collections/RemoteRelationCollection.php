ed282ed04d748c7c6dbfd37e419edc7f9972c48a
Marko Dupor <marko.dupor@asseco-see.hr>
2022-03-09 15:15:13 +0100

    Batched resolution + facade support

--- src/App/Collections/RemoteRelationCollection.php
+++ src/App/Collections/RemoteRelationCollection.php
@@ -9,6 +9,58 @@ use Illuminate\Database\Eloquent\Collection;
 
 class RemoteRelationCollection extends Collection
 {
+    protected const DATA = 'data';
+    protected const RESOLUTION = 'resolution';
+
+    /**
+     * Overriding Laravel append method to support batching HTTP calls to external services
+     *
+     * @param array|string $attributes
+     * @return RemoteRelationCollection
+     */
+    public function append($attributes)
+    {
+        if ($this->resolutionInString($attributes)) {
+            $this->batchResolutions();
+
+            return $this;
+        }
+
+        if ($this->resolutionInArray($attributes)) {
+            $this->batchResolutions();
+
+            if (($key = array_search(self::RESOLUTION, $attributes)) !== false) {
+                unset($attributes[$key]);
+            }
+        }
+
+        return parent::append($attributes);
+    }
+
+    protected function resolutionInString(string $attributes): bool
+    {
+        return is_string($attributes) && $attributes == self::RESOLUTION;
+    }
+
+    protected function resolutionInArray(string $attributes): bool
+    {
+        return is_array($attributes) && in_array(self::RESOLUTION, $attributes);
+    }
+
+    protected function batchResolutions(): void
+    {
+        $resolvedRelations = (new RelationsResolver())->resolveRelations($this);
+
+        $this->each(function ($remoteRelation) use ($resolvedRelations) {
+
+            $data = array_filter($resolvedRelations, function ($resolvedRelation) use ($remoteRelation) {
+                return $resolvedRelation['id'] == $remoteRelation->remote_model_id;
+            });
+
+            $remoteRelation->{self::DATA} = array_pop($data) ?: [];
+        });
+    }
+
     public function resolve()
     {
         $resolvedRelations = (new RelationsResolver())->resolveRelations($this);
@@ -18,7 +70,7 @@ class RemoteRelationCollection extends Collection
 
             return array_merge(
                 optional($originalRelationObject)->toArray() ?: [],
-                ['data' => $resolvedRelation]
+                [self::DATA => $resolvedRelation]
             );
         }, $resolvedRelations);
     }
